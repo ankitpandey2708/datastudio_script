ee832399a790ef4d61b3729fd3cad8f14f2acb08
Ankit Pandey <ankitpandey2708@gmail.com>
2020-08-11 03:44:34 +0530

    Update ubtan.py

--- ubtan.py
+++ ubtan.py
@@ -1,5 +1,3 @@
-import locale
-locale.setlocale( locale.LC_ALL,"en_US.UTF-8")
 import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -9,7 +7,7 @@ driver.get(url)
 time.sleep(5)
 elem = driver.page_source.encode('utf-8')
 html_soup = BeautifulSoup(elem, 'html.parser')
-a=html_soup.find_all('div', {'class': 'valueLabel ng-binding'})
+a=html_soup.find_all('div', {'class': 'valueLabel'})
 Impressions = int(a[0].text.split()[0].replace(',',''))
 Clicks = int(a[1].text.split()[0].replace(',',''))
 print(Impressions)
